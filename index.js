04c84f9728abcd46b6af9ab03b9d965260665f9b
Aman Kothari <kothariaman2705@gmail.com>
2025-03-10 20:49:41 +0530

    Fixed JSON response issue and updated frontend API handling

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const express =require("express")
+const shortId =require('shortid')
 const cors = require("cors")
-const  urlRoute = require('./routes/url')
+const URL = require("./schema")
 const app = express()
 const {ConnectToDB} = require('./connect')
 const PORT = process.env.PORT || 3000
@@ -9,14 +10,42 @@ app.use(cors({
     origin: "*" ,
     credentials: true
 }));
-
-const {handleGetAnalytics} = require('./controllers/url')
 ConnectToDB()
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-app.use('/url',urlRoute)
-app.get('/:shortId',handleGetAnalytics)
+app.post('/',async(req,res)=>{
+    try {
+        const url = req.body.url
+        const shortID = shortId()
+        if(!url)return res.status(400).json({error:'url not found'})
+        
+        await URL.create({
+          shortId:shortID,
+          redirectUrl:url,
+          visitHistory:[],
+        })
+        return res.status(201).json({message:'url created successfully',shortID})
+    } catch (error) {
+       return res.status(500).json({error:'error occured while generating short id'})
+    }
+   })
+   
+  app.get('/:shortId',async(req,res)=>{
+     try {
+       const shortId = req.params.shortId
+       if(!shortId)return res.status(400).json({error:"shortId not found"})
+       const entry = await URL.findOneAndUpdate(
+       {shortId},
+       {$push:{visitHistory:{Timestamp:Date.now()}}},
+       {new:true},
+       )
+       if(!entry)return res.status(400).json({error:"shortId not found "})
+         return res.redirect(entry.redirectUrl)
+     } catch (error) {
+       return res.status(500).json({error:'error occured while generating short id'})
+     }
+   })
 app.get('/',(req,res)=>{
     return res.send("hello from server")
 })
--- routes/url.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-
-const router = express.Router()
-const {handleGenerateShortId,handleRedirectURL} = require('../controllers/url')
-router.post('/',handleGenerateShortId)
-router.get('/:shortId',handleRedirectURL)
-module.exports = router
